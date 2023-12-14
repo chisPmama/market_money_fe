e506c3ef4424372052848b6211e7f42b2eee536b
Anthea Yur <acyur6@gmail.com>
2023-12-14 11:55:30 -0600

    Remove comments

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,18 +1,5 @@
 class MarketsController < ApplicationController
   def index
-    # Sending the lat and lon properties to the API
-    # conn = Faraday.new(url: "http://localhost:3000")
-
-    ## Service
-    # def get_markets
-    # response = conn.get("/api/v0/markets")
-    # end
-
-    # data = JSON.parse(response.body, symbolize_names: true)[:data]
-    # @markets = data.map do |d|
-    #   Market.new(d)
-    # end
-
     @facade = MarketFacade.new
     @markets = @facade.get_markets
   end
